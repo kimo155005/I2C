a9581a6c710264cd8c16e018ab10043f5c4ff0cb
kimo155005 <kareennohamed334@gmail.com>
2025-07-28 13:31:18 +0300

    main.c

--- I2C_master/main.c
+++ I2C_master/main.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    u8 name[] = "KAREEM";  // لازم يحتوي على أكثر من حرف
+    u8 name[] = "KAREEM";  
     u8 i = 0;
 
     I2C_voidMasterInit(0);
@@ -16,14 +16,14 @@ int main(void)
         if (I2C_Master_u8SendSLA_Write_ACK(0x20) == I2C_NO_Error)
         {
             i = 0;
-            while (name[i] != '\0')  // لازم يبعث كل الحروف
+            while (name[i] != '\0')  
             {
                 I2C_Master_u8WriteDataByte_ACK(name[i++]);
             }
-            I2C_Master_u8WriteDataByte_ACK('\0');  // نهاية الرسالة
+            I2C_Master_u8WriteDataByte_ACK('\0');  
         }
 
         I2C_Master_voidStopCondition();
-        _delay_ms(3000);  // علشان تشوف البيانات وتعيد الإرسال
+        _delay_ms(3000);  
     }
 }
